ea07aa4343ae41389c7b24c49c601dab2d2eeeb3
Lanceconan <conan_022@hotmail.com>
2018-03-21 00:50:22 -0300

    CRUD en primera tabla
    Uso de framework entity para creación de una tabla en la base de datos y
    crud simple con mejoras en las interfaces gráficas. Primero
    Modelo-Vistas-Controlador.

---
+++ Sistema/Data/DbInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sistema.Models;
+
+namespace Sistema.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(SistemaContext context)
+        {
+            /*Se crea con este metodo la base de datos*/
+            context.Database.EnsureCreated();
+
+            //Buscar si existen registro en la tabla categoría
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+
+            //si la base de datos está vacía va a llenar con registros la base de datos
+            var categorias = new Categoria[]
+            {
+                new Categoria{ Nombre = "Programación", Descripcion = "Curso de Programación", Estado = true},
+                new Categoria{ Nombre = "Diseño Gráfico", Descripcion = "Curso de Diseño Gráfico", Estado = true}
+            };
+
+            //Se recorre el array con los nuevos datos para la Base de Datos y se inserta uno a uno los registros
+            foreach (Categoria C in categorias)
+            {
+                context.Categoria.Add(C);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
